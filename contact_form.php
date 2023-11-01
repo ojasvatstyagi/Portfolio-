f3cd9a0e095e2b15cb36b12f5242f82e3dc87817
Ojas vats Tyagi <137185464+ojasvatstyagi@users.noreply.github.com>
2023-11-01 22:48:21 +0530

    Add files via upload

---
+++ contact_form.php
@@ -0,0 +1,34 @@
+<?php
+// Database configuration
+$servername = "localhost";
+$username = "root";
+$database = "James";
+
+// Create a connection to the MySQL database
+$conn = new mysqli($servername, $username,$database);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Process the form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["Name"];
+    $email = $_POST["Email"];
+    $message = $_POST["Message"];
+
+    // SQL query to insert data into the database
+    $sql = "INSERT INTO your_table_name (Name, Email, Message) VALUES ('$name', '$email', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Data inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Close the database connection
+$conn->close();
+?>
+
